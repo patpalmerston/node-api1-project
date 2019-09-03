e6aa4446849ffd8b578c52f8c6559a16801447cb
Pat Palmerston <patpalmerston@gmail.com>
2019-09-02 21:46:55 -0700

    findById

--- index.js
+++ index.js
@@ -44,16 +44,26 @@ server.get('/api/users', (req, res) => {
 });
 
 // Get user by Id
-server.get('/api/users/id', (req, res) => {
+server.get('/api/users/:id', (req, res) => {
 	const id = req.params.id;
 
 	data
 		.findById(id)
 		.then(user => {
-			res.status(200).json(user);
+			if (user) {
+				res.status(200).json({
+					user
+				});
+			} else {
+				res.status(404).json({
+					message: 'The user with the specified ID does not exist.'
+				});
+			}
 		})
 		.catch(err => {
-			res.status(500).json({ message: 'user info unavailable' });
+			res.status(500).json({
+				error: 'The user information could not be retrieved.'
+			});
 		});
 });
 
