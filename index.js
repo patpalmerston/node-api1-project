7035b20601095c4d5ba1ca531181fadc9f16169e
Pat Palmerston <patpalmerston@gmail.com>
2019-09-02 21:11:35 -0700

    delete

--- index.js
+++ index.js
@@ -43,8 +43,22 @@ server.get('/api/users', (req, res) => {
 		});
 });
 
+// Get user by Id
+server.get('/api/users/id', (req, res) => {
+	const id = req.params.id;
+
+	data
+		.findById(id)
+		.then(user => {
+			res.status(200).json(user);
+		})
+		.catch(err => {
+			res.status(500).json({ message: 'user info unavailable' });
+		});
+});
+
 // Delete a user
-server.delete('/api/user/:id', (req, res) => {
+server.delete('/api/users/:id', (req, res) => {
 	const userId = req.params.id;
 
 	data
