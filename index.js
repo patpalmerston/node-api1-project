8bc22dd2328b4c4dac443f4ac6f52a9058ef202a
Pat Palmerston <patpalmerston@gmail.com>
2019-09-02 20:52:38 -0700

    add is not a function

--- index.js
+++ index.js
@@ -2,21 +2,34 @@
 const express = require('express');
 
 // import db folder
-const db = require('./data/db');
+const data = require('./data/db');
 
 const server = express();
+
+server.use(express.json());
 
 server.get('/', (req, res) => {
 	res.send('Hello from Express');
 });
 
+// create user
 server.post('/api/users', (req, res) => {
-	res.send();
+	const userData = req.body;
+	console.log('userData', userData);
+	data
+		.add(userData)
+		.then(user => {
+			res.status(201).json(user);
+		})
+		.catch(err => {
+			res.status(500).json({ message: 'error adding user' });
+		});
 });
 
 server.get('/api/users', (req, res) => {
 	// db.find() returns a promose and we need then and catch
-	db.find()
+	data
+		.find()
 		.then(users => {
 			// .json will convert hte data passed to json
 			// also tells the client we're sending JSON through and HTTP header
@@ -24,6 +37,20 @@ server.get('/api/users', (req, res) => {
 		})
 		.catch(err => {
 			res.status(500).json({ message: 'error getting users' });
+		});
+});
+
+// Delete a user
+server.delete('/api/user/:id', (req, res) => {
+	const userId = req.params.id;
+
+	data
+		.remove(userId)
+		.then(user => {
+			res.status(200).json({ message: 'user deleted' });
+		})
+		.catch(err => {
+			res.status(500).json({ message: 'error removing user' });
 		});
 });
 
